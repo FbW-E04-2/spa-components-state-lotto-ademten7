a9d303b3443f1f6c6c81374db4b3555f55927b22
ademten7 <82845552+ademten7@users.noreply.github.com>
2021-11-02 22:49:17 +0100

    Update Lotto.js

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,75 +1,71 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { createLuckyNumbers, createSpecialNumber } from "./numbers";
 
-const generateNumbers = () => {
-  let luckyNumbers = [];
+function Lotto(props) {
+  const generateNumbers = () => {
+    let luckyNumbers = [];
 
-  let randomNumber;
-  let counter = 0;
-  while (counter < 6) {
-    randomNumber = createLuckyNumbers();
-    if (luckyNumbers.includes(randomNumber)) {
-      continue;
-    } else {
-      luckyNumbers = [...luckyNumbers, randomNumber];
-      counter++;
+    let randomNumber;
+    let counter = 0;
+    while (counter < 6) {
+      randomNumber = createLuckyNumbers();
+      if (luckyNumbers.includes(randomNumber)) {
+        continue;
+      } else {
+        luckyNumbers = [...luckyNumbers, randomNumber];
+        counter++;
+      }
     }
-  }
-  return luckyNumbers;
-};
-
-export default class Lotto extends Component {
-  state = {
-    luckyNumbersOfState: [],
-    specialNumber: [],
+    return luckyNumbers;
   };
 
-  getNewNumbers = () => {
-    this.setState({
-      luckyNumbersOfState: generateNumbers(),
-      specialNumber: createSpecialNumber(),
-      show: "block",
-    });
+  const [luckyNumbersOfState, setLuckyNumbersOfState] = useState([]);
+  const [specialNumber, setSpecialNumber] = useState([]);
+  const [show, setShow] = useState("");
+
+  const getNewNumbers = () => {
+    setLuckyNumbersOfState(generateNumbers());
+    setSpecialNumber(createSpecialNumber());
+    setShow("block");
   };
 
-  resetNumbers = () => {
-    this.setState({
-      luckyNumbersOfState: [],
-      specialNumber: [],
-      show: "none",
-    });
+  const resetNumbers = () => {
+    setLuckyNumbersOfState([]);
+    setSpecialNumber([]);
+    setShow("none");
   };
 
-  render() {
-    console.log(this.state.luckyNumbersOfState);
-    console.log("special" + this.state.specialNumber);
+  console.log(luckyNumbersOfState);
+  console.log("special" + specialNumber);
 
-    return (
-      <div className="container">
-        <h1 style={{ textAlign: "center" }}>{this.props.header}</h1>
-        <p style={{ textAlign: "center" }}>{this.props.para}</p>
-        <ul className="numbers">
-          {this.state.luckyNumbersOfState.map((num) => {
-            return (
-              <li className="random-numbers" key={num}>
-                <span>{num}</span>
-              </li>
-            );
-          })}
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>{props.header}</h1>
+      <p style={{ textAlign: "center" }}>{props.para}</p>
+      <ul className="numbers">
+        {luckyNumbersOfState.map((num) => {
+          return (
+            <li className="random-numbers" key={num}>
+              <span>{num}</span>
+            </li>
+          );
+        })}
 
-          <li style={{ display: this.state.show }} className="specialNumber">
-            {this.state.specialNumber}
-          </li>
-        </ul>
-        <div className="buttons ">
-          <button className="reset" onClick={this.resetNumbers}>
-            Reset
-          </button>
-          <button className="show-time" onClick={this.getNewNumbers}>
-            Show me lucky numbers
-          </button>
-        </div>
+        <li style={{ display: show }} className="specialNumber">
+          {specialNumber}
+        </li>
+      </ul>
+      <div className="buttons ">
+        <button className="reset" onClick={resetNumbers}>
+          Reset
+        </button>
+        <button className="show-time" onClick={getNewNumbers}>
+          Show me lucky numbers
+        </button>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+export default Lotto;
+
